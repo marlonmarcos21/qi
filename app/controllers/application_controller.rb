31098f8fb097340084d38f05301bef559ae969bf
Marlon Marcos <marlon@Marlons-MacBook-Pro.local>
2017-09-07 18:13:01 +0800

    Set google api key as an environment variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  helper_method :google_api_key
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to new_user_session_path, alert: exception.message
+  end
+
+  def google_api_key
+    ENV['GOOGLE_API_KEY']
   end
 
   protected
